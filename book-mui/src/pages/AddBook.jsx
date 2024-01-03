761901c1bdd9c96e442aec727e1dd5e2b96a4329
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-03 12:37:27 +0000

    Removed header from add book page

--- book-mui/src/pages/AddBook.jsx
+++ book-mui/src/pages/AddBook.jsx
@@ -3,7 +3,6 @@ import { AddBookForm } from "../components/AddBookForm";
 export const AddBook = () => {
   return (
     <>
-      <h1>Add Book</h1>
       <div style={{ alignContent: "center", justifyContent: "center" }}>
         <AddBookForm />
       </div>
