e13d698fc739d1df12f3368f509c403ff6810a9b
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-03 12:36:00 +0000

    Created Add Book Form

--- book-mui/src/components/AddBookForm.jsx
+++ book-mui/src/components/AddBookForm.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import FormGroup from "@mui/material/FormGroup";
 import FormControl from "@mui/material/FormControl";
-import { TextField, Stack, Button } from "@mui/material";
+import { TextField, Stack, Button, Typography } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
 
-export const AddBookForm = () => {
+export const AddBookForm = (props) => {
   const url = "http://localhost:3000/api/book/";
+  const getUrl = url + props.bookId;
+
+  console.log("Get " + getUrl);
   const [post, setPost] = useState({
     authorFirstName: "",
     authorLastName: "",
@@ -30,39 +33,50 @@ export const AddBookForm = () => {
   return (
     <>
       <form method="POST" onSubmit={handleSubmit}>
-        <Stack spacing={2} width={900}>
+        <Stack
+          spacing={2}
+          width={900}
+          sx={{ marginLeft: "30%", margiTop: "5%" }}
+        >
+          <Typography>Add Book</Typography>
           <TextField
             label="Title"
             name="title"
             type="text"
+            defaultValue={props.title}
             onChange={handleInput}
           />
           <TextField
             label="Author First Name"
             name="authorFirstName"
             type="text"
+            defaultValue={props.authorFirstName}
             onChange={handleInput}
           />
           <TextField
             label="Author Last Name"
             name="authorLastName"
             type="text"
+            defaultValue={props.authorLastName}
             onChange={handleInput}
           />
           <TextField
             label="Blurb"
             name="blurb"
             type="text"
+            placeholder={props.blurb}
+            defaultValue={props.blurb}
             onChange={handleInput}
           />
           <TextField
             label="Image URL"
             name="imgUrl"
             type="text "
+            defaultValue={props.imgUrl}
             onChange={handleInput}
           />
           <Button
-            type="submit"
+            type="Update"
             variant="contained"
             color="success"
             onChange={handleInput}
