2e88dbd98188463213a17029bb987d5b43e9a36a
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-03 12:36:26 +0000

    Added route for edit books page

--- book-mui/src/App.jsx
+++ book-mui/src/App.jsx
@@ -12,6 +12,7 @@ import "./App.css";
 import { ManageBooksButton } from "./components/ManageBooksButton";
 import { Route, Routes } from "react-router-dom";
 import { AddBook } from "./pages/AddBook";
+import { EditBookPage } from "./pages/EditBookPage";
 import { Manage } from "./pages/Manage";
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/dashboard/view-books" element={<ViewBooks />} />
         <Route path="/dashboard/manage" element={<Manage />} />
         <Route path="/dashboard/add-book" element={<AddBook />} />
+        <Route path="/dashboard/manage/edit/:id" element={<EditBookPage />} />
       </Routes>
     </>
   );
