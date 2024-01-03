4d49dc3187114c678204d9e0da1c55f3e079610b
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-03 12:49:58 +0000

    Created front end page (main page)

---
+++ book-mui/src/pages/front-end/BooksPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FrontEndNav } from "../../components/front-end/FrontEndNav";
+import { BookCard } from "../../components/front-end/BookCard";
+export const BooksPage = () => {
+  return (
+    <>
+      <BookCard />
+    </>
+  );
+};
