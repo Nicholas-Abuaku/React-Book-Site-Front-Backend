d3d61dd0847cbb84f1bb6aef9baa330677964010
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-05 11:45:53 +0000

    changed margin for button grid

--- book-mui/src/components/ButtonGrid.jsx
+++ book-mui/src/components/ButtonGrid.jsx
@@ -5,23 +5,25 @@ import { DashboardViewButton } from "./DashboardViewButton";
 import { Grid, Stack, Box, Typography } from "@mui/material";
 export const ButtonGrid = () => {
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{ border: "1px solid", width: "40%", margin: "auto" }}
-    >
-      <Grid item xs={5}>
-        <DashboardViewButton />
+    <>
+      <Grid
+        container
+        spacing={2}
+        sx={{ width: "40%", margin: "0", marginRight: "30px" }}
+      >
+        <Grid item xs={5}>
+          <DashboardViewButton />
+        </Grid>
+        <Grid item xs={7}>
+          <DashboardManageButton />
+        </Grid>
+        <Grid item xs={5}>
+          <DashboardAddBookButtn />
+        </Grid>
+        <Grid item xs={7}>
+          <DashboardAddBookButtn />
+        </Grid>
       </Grid>
-      <Grid item xs={7}>
-        <DashboardManageButton />
-      </Grid>
-      <Grid item xs={5}>
-        <DashboardAddBookButtn />
-      </Grid>
-      <Grid item xs={7}>
-        <DashboardAddBookButtn />
-      </Grid>
-    </Grid>
+    </>
   );
 };
