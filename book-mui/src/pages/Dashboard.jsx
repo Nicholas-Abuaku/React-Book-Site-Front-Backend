89d20a370e59cf8aebf9160eb7c37e4720b06c2d
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-05 11:46:59 +0000

    minor styling edits to dashboard

--- book-mui/src/pages/Dashboard.jsx
+++ book-mui/src/pages/Dashboard.jsx
@@ -1,16 +1,27 @@
-import { Box, Grid, Stack } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import { DashboardInfo } from "../components/DashboardInfo";
 import { ButtonGrid } from "../components/ButtonGrid";
 import { DashboardAddBookButtn } from "../components/DashboardAddBookButtn";
 import { DashboardViewButton } from "../components/DashboardViewButton";
 import { DashboardManageButton } from "../components/DashboardManageButton";
+import { BackEndSideBar } from "../components/BackEndSideBar";
+import { RecentBooks } from "../components/RecentBooks";
 export const Dashboard = () => {
   return (
     <>
       <Box>
-        <DashboardInfo />
-        <ButtonGrid />
-        <DashboardInfo />
+        <BackEndSideBar />
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          minHeight={"100vh"}
+          sx={{ border: "1px solid" }}
+        >
+          <ButtonGrid />
+          <RecentBooks />
+          <DashboardInfo />
+        </Box>
       </Box>
     </>
   );
