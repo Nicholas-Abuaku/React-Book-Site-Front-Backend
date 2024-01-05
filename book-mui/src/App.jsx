07776fd954eabd70e747e761135998522f574f91
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-05 17:44:41 +0000

    Removed Dashboard, replaced with different file (same thing better layout)

--- book-mui/src/App.jsx
+++ book-mui/src/App.jsx
@@ -4,7 +4,6 @@ import { DashboardViewButton } from "./components/DashboardViewButton";
 import { DashboardAddBookButtn } from "./components/DashboardAddBookButtn";
 import { DashboardInfo } from "./components/DashboardInfo";
 import { ButtonGrid } from "./components/ButtonGrid";
-import { Dashboard } from "./pages/Dashboard";
 import { AllBooksTable } from "./components/AllBooksTable";
 import { ManageTable } from "./components/ManageTable";
 import { ViewBooks } from "./pages/ViewBooks";
@@ -16,11 +15,12 @@ import { EditBookPage } from "./pages/EditBookPage";
 import { Manage } from "./pages/Manage";
 import { BooksPage } from "./pages/front-end/BooksPage";
 import { BackEndSideBar } from "./components/BackEndSideBar";
+import { Dashboard2 } from "./pages/Dashboard2";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard2 />} />
         <Route path="/dashboard/view-books" element={<ViewBooks />} />
         <Route path="/dashboard/manage" element={<Manage />} />
         <Route path="/dashboard/add-book" element={<AddBook />} />
---
+++ book-mui/src/pages/Dashboard2.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import { DashboardNav } from "../components/DashboardNav";
+import { BackEndSideBar } from "../components/BackEndSideBar";
+import { ButtonGrid } from "../components/ButtonGrid";
+import { RandomBook } from "../components/RandomBook";
+import { DashboardInfo } from "../components/DashboardInfo";
+export const Dashboard2 = () => {
+  return (
+    <Grid
+      container
+      sx={{ border: "1px solid" }}
+      width={"100%"}
+      height={"100%"}
+      direction={"row"}
+    >
+      <Grid item xs={12}>
+        <BackEndSideBar />
+      </Grid>
+      <Grid item xs={12}>
+        <DashboardNav />
+      </Grid>
+      <Grid item xs={12} textAlign={"center"}>
+        <Typography variant="h4">Admin Panel</Typography>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={12}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        sx={{
+          border: "5px solid",
+          height: "100%",
+          marginLeft: "80px",
+          marginTop: "10px",
+        }}
+      >
+        <DashboardInfo />
+        <ButtonGrid />
+        <RandomBook />
+      </Grid>
+    </Grid>
+  );
+};
