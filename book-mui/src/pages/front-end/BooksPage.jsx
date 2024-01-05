566114a4a208344dd35f48c956afab66b0bc30e0
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-01-05 18:43:50 +0000

    Book page now fetches from api

--- book-mui/src/pages/front-end/BooksPage.jsx
+++ book-mui/src/pages/front-end/BooksPage.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { FrontEndNav } from "../../components/front-end/FrontEndNav";
 import { BookCard } from "../../components/front-end/BookCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
 export const BooksPage = () => {
+  const [allBooks, setAllBooks] = useState([]);
+  const url = "http://localhost:3000/api/book/";
+  const updateBooks = async () => {
+    try {
+      const res = await axios.get(url);
+      console.log(res.data.books);
+      setAllBooks(res.data.books);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    updateBooks();
+  }, []);
   return (
     <>
       <FrontEndNav />
-      <BookCard />
+      <Grid
+        container
+        direction={"row"}
+        spacing={4}
+        marginLeft={2}
+        marginTop={2}
+      >
+        {allBooks.map((book) => (
+          <Grid item xs={2}>
+            <BookCard
+              authorFirstName={book.authorFirstName}
+              title={book.title}
+              authorLastName={book.authorLastName}
+              img={book.imgUrl}
+              blurb={book.blurb}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 };
